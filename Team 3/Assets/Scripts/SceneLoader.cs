22bd0f6d1f7db966fe7f2b83e24420a2cf82cde8
MUKILTEO\1721488 <1721488@mukilteo.wednet.edu>
2024-11-25 08:16:54 -0800

    made title, and A.I

---
+++ Team 3/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("GameLevelOne");
+    }
+    public void GetOut()
+    {
+        Application.Quit();
+    }
+}
