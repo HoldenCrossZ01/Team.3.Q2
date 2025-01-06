60ca1459f700ad0ef3121571cefe1dcef9390203
MUKILTEO\1721488 <1721488@mukilteo.wednet.edu>
2025-01-06 10:19:12 -0800

    A.I nearly done

--- Team 3/Assets/Gabe stuff dont mess with it/MoveSpeed.cs
+++ Team 3/Assets/Gabe stuff dont mess with it/MoveSpeed.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class MoveSpeed : MonoBehaviour
 {
-public float moveSpeed;
-public Rigidbody2D rb;
+    public Animator anim;
 
-public float x;
-public float y;
+public float moveSpeed;
+private Rigidbody2D rb;
+
+private float x;
+private float y;
+
+    private Vector2 input;
+    private bool Moving;
     void Start()
     {
 
     }
     private void Update()
     {
-        GetInput();   
+        GetInput();
+        Animate();
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(x * moveSpeed,y * moveSpeed);
+        rb.velocity = input  * moveSpeed;
     }
 
     private void GetInput()
     {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
+
+        input = new Vector2(x, y);
+        input.Normalize();
+    }
+    private void Animate()
+    {
+        if(input.magnitude >0.1f || input.magnitude < -0.1f)
+        {
+            Moving = true;
+        }
+        else {Moving = false; }
+
+        if (Moving)
+        {
+            anim.SetFloat("X", x);
+            anim.SetFloat("Y", y);
+        }
+
+        anim.SetBool("Moving", Moving);
     }
 }
