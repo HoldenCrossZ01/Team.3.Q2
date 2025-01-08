259c359f125c3583496c41397304b443e06ac294
MUKILTEO\1721488 <1721488@mukilteo.wednet.edu>
2025-01-08 07:57:41 -0800

    A.I nearly done

--- Team 3/Assets/Gabe stuff dont mess with it/MoveSpeed.cs
+++ Team 3/Assets/Gabe stuff dont mess with it/MoveSpeed.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Lumin;
 
 public class MoveSpeed : MonoBehaviour
 {
@@ -11,37 +12,50 @@ private Rigidbody2D rb;
 
 private float x;
 private float y;
-
+    private float startposition;
     private Vector2 input;
     private bool Moving;
+    private Vector2 oldposition;
+    private Vector2 newposition;
     void Start()
     {
 
     }
     private void Update()
     {
+
+        
+        newposition = transform.position;
+        
         GetInput();
+        
+
         Animate();
+        startposition = Vector2.Distance(newposition, oldposition);
+        oldposition = transform.position;
+
     }
     private void FixedUpdate()
     {
-        rb.velocity = input  * moveSpeed;
+          rb.velocity = input  * moveSpeed;
     }
 
     private void GetInput()
     {
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
+       // x = Input.GetAxisRaw("Horizontal");
+       x = this.gameObject.transform.position.x; //fix
+      //  y = Input.GetAxisRaw("Vertical");
+       y = this.gameObject.transform.position.y; //fix
 
         input = new Vector2(x, y);
         input.Normalize();
     }
     private void Animate()
     {
-        if(input.magnitude >0.1f || input.magnitude < -0.1f)
-        {
-            Moving = true;
-        }
+        if(newposition != oldposition) //|| input.magnitude < -0.1f)
+           {
+                Moving = true;
+             } 
         else {Moving = false; }
 
         if (Moving)
@@ -52,4 +66,17 @@ private float y;
 
         anim.SetBool("Moving", Moving);
     }
+
+    private IEnumerator CheckMoving()
+    {
+        Vector3 startPos = this.gameObject.transform.position;
+        yield return new WaitForSeconds(1f);
+        Vector3 finalPos = this.gameObject.transform.position;
+
+        if (startPos.x != finalPos.x || startPos.y != finalPos.y
+            || startPos.z != finalPos.z)
+            Moving = true;
+
+    }
+
 }
