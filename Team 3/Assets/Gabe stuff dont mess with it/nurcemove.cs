a40efe73baf984b671331a44c94ee4105acad278
MUKILTEO\1721488 <1721488@mukilteo.wednet.edu>
2025-01-17 09:36:56 -0800

    A.I done

--- Team 3/Assets/Gabe stuff dont mess with it/nurcemove.cs
+++ Team 3/Assets/Gabe stuff dont mess with it/nurcemove.cs
@@ -1,3 +1,4 @@
+using Pathfinding.Ionic.Zip;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class nurcemove : MonoBehaviour
 
     private float x;
     private float y;
-    private bool Possesed;
+    private bool Possesed = false;
     private float startposition;
     private Vector2 input;
     private bool Moving;
@@ -29,7 +30,6 @@ public class nurcemove : MonoBehaviour
         
         GetInput();
 
-        possesion();
         Animate();
         
         startposition = Vector2.Distance(newposition, oldposition);
@@ -53,7 +53,7 @@ public class nurcemove : MonoBehaviour
     }
     private void Animate()
     {
-        if (input.magnitude > 0.1f || input.magnitude < -0.1f) //|| input.magnitude < -0.1f)
+        if (newposition != oldposition) //|| input.magnitude < -0.1f)
         {
             Moving = true;
         }
@@ -68,13 +68,5 @@ public class nurcemove : MonoBehaviour
         anim.SetBool("Moving", Moving);
 
 
-    }
-    private void possesion()
-    {
-        anim.SetBool("possesed", Possesed);
-        if (Input.GetKey(KeyCode.L))
-        {
-            Possesed = true;
-        }
     }
 }
