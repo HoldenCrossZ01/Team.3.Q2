70f8b345cdf93306273c861c5b53397b974cd022
MUKILTEO\1702249 <1702249@mukilteo.wednet.edu>
2025-01-17 10:15:58 -0800

    skibidi

--- Team 3/Assets/Joshua.Z/My Scripts/CamClamp.cs
+++ Team 3/Assets/Joshua.Z/My Scripts/CamClamp.cs
@@ -13,7 +13,7 @@ public class CamClamp : MonoBehaviour
     {
         transform.position = new Vector3(
             Mathf.Clamp(targetToFollow.position.x, -10000, 10000),
-            Mathf.Clamp(targetToFollow.position.y, 10, 15),
+            Mathf.Clamp(targetToFollow.position.y, 4, 15),
             transform.position.z);
 
     }
